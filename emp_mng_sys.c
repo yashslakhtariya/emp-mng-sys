bbc6423907375054b586681a0c7edb4e7221793e
yashslakhtariya <98678102+yashslakhtariya@users.noreply.github.com>
2022-01-30 12:24:30 +0530

    Update emp_mng_sys.c

--- emp_mng_sys.c
+++ emp_mng_sys.c
@@ -2,22 +2,6 @@
 #include <stdlib.h>
 #include <string.h>  ///string operations
 
-/** List of Global Variable 
-COORD coord = {0,0}; /// top-left corner of window
-*/
-
-/*/**
-    function : gotoxy
-    @param input: x and y coordinates
-    @param output: moves the cursor in specified position of console
-
-void gotoxy(int x,int y)
-{
-    coord.X = x;
-    coord.Y = y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
-}
-*/
 /** Main function started */
 
 int main()
